0aa17ac32941d50035bad7f26dac8109321b72e4
abduldocxster <162561204+abduldocxster@users.noreply.github.com>
2024-09-27 17:56:36 +0530

    Added Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./ui/globals.css";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
@@ -8,10 +9,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
-        <Navbar/>
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow">{children}</div>
+        <Footer />
       </body>
     </html>
   );
